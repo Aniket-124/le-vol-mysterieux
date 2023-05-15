6acd8b1f5eb2fc33b5c41612b5b140b68d5c90cb
Aniket Murjani <aniketmurjani4Wgmail.com>
2023-05-16 00:54:24 +0530

    add game page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Game from './pages/Game';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={(
+          <Game/>
+        )} />
+        <Route path='*' element={(
+          <div>
+            <h1>404 - Not found</h1>
+          </div>
+        )} />
+      </Routes>
+    </BrowserRouter> 
+  )
 }
 
 export default App;
